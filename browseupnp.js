ad56ad8d8467acba29cb9d1311e930173b370f9c
volumio <info@volumio.org>
2017-07-07 18:06:35 +0000

    further work

--- browseupnp.js
+++ browseupnp.js
@@ -8,11 +8,9 @@ client.search('urn:schemas-upnp-org:device:MediaServer:1')
 
 
 client.on('response', function inResponse(headers, code, rinfo) {
-    //console.log('Got a response to an m-search:\n%d\n%s\n%s', code, JSON.stringify(headers, null, '  '), JSON.stringify(rinfo, null, '  '))
-    //console.log('CODE: '+code)
-    //console.log('HEADERS: '+JSON.stringify(headers, null, '  '));
-    //console.log('INFO: '+ JSON.stringify(rinfo, null, '  '))
-    addUpnpServer(headers);
+    var urlraw = headers.LOCATION.replace('http://', '').split('/')[0].split(':');
+    var server = {'url': 'http://'+urlraw[0], 'port': urlraw[1], 'endpoint': headers}
+    addUpnpServer(server);
 })
 
 setTimeout(function() {
@@ -21,20 +19,25 @@ setTimeout(function() {
 }, 30000)
 
 
-function addUpnpServer(headers) {
-    var UpnpHeaders = headers
-    xmlToJson(headers.LOCATION, function(err, data) {
+function addUpnpServer(server) {
+    xmlToJson(server.endpoint.LOCATION, function(err, data) {
         if (err) {
             // Handle this however you like
             return console.err(err);
         }
+        console.log(data.host);
         var headers = headers;
         var upnpServerName = data.device.friendlyName;
         var upnpServerUDN = data.device.udn;
         var upnpServerIcon = data.device.iconList.icon[0].url;
 
-        console.log(upnpServerName)
+        var upnpServer = {'name': upnpServerName, 'UDN':upnpServerUDN, 'icon': upnpServerUDN}
+
+        //console.log(upnpServerName)
+        //console.log(upnpServerUDN)
+        //console.log(upnpServerIcon)
         console.log(JSON.stringify(data, null, 2));
+        //console.log(JSON.stringify(upnpServer));
     });
 }
 
